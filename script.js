4eefa27589c10320609d93299389b3347edea6a9
Artem <schwets19072009@gmail.com>
2025-03-04 20:51:45 +0300

    1

--- script.js
+++ script.js
@@ -1,54 +1,31 @@
-let i = 1;
+const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
+let string = friends.join(", ");
+console.log(string);
 
-while (i <= 10) {
-  console.log(i);
-  i++;
+const cards = [
+  "Карточка-1",
+  "Карточка-2",
+  "Карточка-3",
+  "Карточка-4",
+  "Карточка-5",
+];
+
+const cardToRemove = "Карточка-3";
+const indexToRemove = cards.indexOf(cardToRemove);
+if (indexToRemove !== -1) {
+  cards.splice(indexToRemove, 1);
 }
+console.log(cards);
 
-for (let i = 2; i <= 20; i++) {
-  if (i % 2 !== 0) {
-    continue;
-  }
-  console.log(i);
+const cardToInsert = "Карточка-6";
+const insertIndex = 2;
+cards.splice(insertIndex, 0, cardToInsert);
+console.log(cards);
+
+const cardToUpdate = "Карточка-4";
+const newCard = "Оновлена карточка-4";
+const indexToUpdate = cards.indexOf(cardToUpdate);
+if (indexToUpdate !== -1) {
+  cards[indexToUpdate] = newCard;
 }
-
-for (let i = 1; i <= 10; i++) {
-  console.log(`7 x ${i} = ${7 * i}`);
-}
-
-const numbers = [1, 2, 3, 4, 5];
-let i = 0;
-
-while (i < numbers.length) {
-  console.log(numbers[i]);
-  i++;
-}
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] === 7) {
-    break;
-  }
-  console.log(numbers[i]);
-}
-
-const n = 10; // Задайте значення n
-
-for (let i = 1; i < 20; i++) {
-  if (i >= n) {
-    break;
-  }
-  console.log(i);
-}
-
-let i = 1;
-
-while (i <= 20) {
-  if (i % 3 === 0) {
-    i++;
-    continue;
-  }
-  console.log(i);   
-  i++;
-}
+console.log(cards);
